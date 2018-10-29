83b82d782da267bcfeb2617c958303e8da65eb25
fanleehao <fanleehao@gmail.com>
2018-10-29 10:48:40 +0800

    change test1 to save result locally

--- dfsIO/test1.py
+++ dfsIO/test1.py
@@ -2,6 +2,34 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
+def tpprint(val, num=100):
+    """
+    Print the first num elements of each RDD generated in this DStream.
+    @param num: the number of elements from the first will be printed.
+    """
+    def takeAndPrint(time, rdd):
+        taken = rdd.take(num + 1)
+        print("########################")
+        print("Time: %s" % time)
+        print("########################")
+        # DATEFORMAT = '%Y%m%d'
+        # today = datetime.datetime.now().strftime(DATEFORMAT)
+        myfile = open("/ttttt.txt", "w")
+        for record in taken[:num]:
+            print(record)
+            myfile.write(str(record)+"\n")
+        myfile.close()
+        if len(taken) > num:
+            print("...")
+        print("")
+
+    val.foreachRDD(takeAndPrint)
+
+
+
+
+
+
 def updateFunction(newValues, runningCount):
     if runningCount is None:
         runningCount = 0
@@ -18,9 +46,12 @@ placecount=places.updateStateByKey(updateFunction)
 #wordcounts.saveAsTextFiles("/tmp/wordcounts")
 
 res=pricesum.join(placecount)
-res.repartition(1).saveAsTextFiles("/tmp/result.txt")
+#res.repartition(1).saveAsTextFiles("/tmp/result.txt")
 res.pprint()
 
+print "\nthis is a line\n"
+tpprint(res)
 
 ssc.start()
 ssc.awaitTermination()
+
